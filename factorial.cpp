9aede21efc2e0235c2410243e5e42dd729b23c32
sameer-saxena <emailsameersaxena@gmail.com>
2016-10-16 19:24:56 +0530

    Factorial Of any number.

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a=1,c;
+	cout<<"Factorial of The NUmber :"<<endl;
+	cin>>c;
+	for(int b=1;b<=c;b++)
+	{
+		a=a*b;
+	}
+	cout<<a<<endl;
+getch();
+}
