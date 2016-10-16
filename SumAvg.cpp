291925441e76b7a6fbb06b3342778b0d7ed9a544
sameer-saxena <emailsameersaxena@gmail.com>
2016-10-16 19:36:10 +0530

    Sum and Average of 100 natural numbers using For

---
+++ SumAvg.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int sum=0,s,a;
+	for(int i=100;i<=100;i--)
+	{
+		s=sum+i;
+	}
+	a=s/100;
+	cout<<"Sum is"<<s<<endl;
+	cout<<"Average is"<<a;
+getch();
+}
