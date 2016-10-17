fad79c8ef464671a30674d21f5b1d8f9541e2b9d
sameer-saxena <emailsameersaxena@gmail.com>
2016-10-17 22:26:59 +0530

    Maximum b/w Numbers by ARRAY

---
+++ ArrayMax.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a[3],max;
+	for (i=0;i<=2;i++)
+	{
+		cin>>a[i];
+	}
+	max=a[0];
+	for(i=0;i<=2;i++)
+	{
+		if( max < a[i] )
+			{
+				max=a[i];
+			}
+	}
+	cout<<max;
+	getch();
+}
